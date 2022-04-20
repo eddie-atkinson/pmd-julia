e3da8e2331229ce09ff2c547cea72d1e21b107e1
Eddie Atkinson <45678264+eddie-atkinson@users.noreply.github.com>
2022-04-20 18:26:21 +0800

    Adds parallelism maybe this will run quicker soon

--- helpers.jl
+++ helpers.jl
@@ -111,8 +111,8 @@ function add_solar_network_model(network_model::Dict{String,Any}, pp_model, netw
             # TODO: actually set this properly
             # qg_lb=[-4.14],
             # qg_ub=[4.14],
-            # qg_lb=[-Inf],
-            # qg_ub=[Inf],
+            qg_lb=[-Inf],
+            qg_ub=[Inf],
             cost_pg_parameters=[0, 0, 0],
         )
         id += 1
@@ -160,7 +160,6 @@ end
 function add_solar_time_series(bus_name_index_map, network_model, pv_df, start_index, end_index)
     time_series = network_model["time_series"]
     pv_rows = pv_df[start_index:end_index, :]
-
     for (solar_id, solar) in network_model["solar"]
         pg_time_series_name = "pg_ub_solar_$solar_id"
 
@@ -214,6 +213,43 @@ function add_solar_power_constraints(model::AbstractUnbalancedPowerModel, data_m
 end
 
 
+function write_results(network_name::String, solution::Dict{String,Any}, output_path::String)
+    bus_index_name_map = get_bus_index_name_map(network_name)
+    bus_name_index_map = Dict(value => key for (key, value) in bus_index_name_map)
+    # Assume we are using multinetwork and have at least one network
+    loads = keys(solution["nw"]["1"])
+
+
+    bus_df = DataFrame(nw=Vector{String}(), bus=Vector{String}(), phase=Vector{Int64}(), voltage=Vector{Float64}())
+    load_df = DataFrame(nw=Vector{String}(), load=Vector{String}(), qd=Vector{Float64}(), pd=Vector{Float64}())
+    pv_df = DataFrame(nw=Vector{String}(), solar=Vector{String}(), qg=Vector{Float64}(), pg=Vector{Float64}())
+    for (network_id, network) in solution["nw"]
+        for (load_name, load) in network["load"]
+            push!(load_df, Dict(:nw => network_id, :load => load_name, :qd => load["qd"][1], :pd => load["pd"][1]))
+        end
+        for (solar_name, solar) in network["solar"]
+            push!(pv_df, Dict(:nw => network_id, :solar => solar_name, :qg => solar["qg"][1], :pg => solar["pg"][1]))
+        end
+
+        for (bus_name, bus) in network["bus"]
+            # Add a row for each phase
+            for i = 1:3
+                phase_vi = bus["vi"][i]
+                phase_vr = bus["vr"][i]
+                phase_voltage = sqrt(phase_vi^2 + phase_vr^2)
+                bus_id = bus_name_index_map[bus_name]
+                push!(bus_df, Dict(:nw => network_id, :bus => "$bus_id", :voltage => phase_voltage, :phase => i))
+            end
+        end
+    end
+    sort!(load_df, [:nw])
+    sort!(pv_df, [:nw])
+    sort!(bus_df, [:nw])
+
+    CSV.write("$output_path-load.csv", load_df)
+    CSV.write("$output_path-pv.csv", pv_df)
+    CSV.write("$output_path-bus.csv", bus_df)
+end
 
 # for i = 1:3
 #     bus_54 = sol_eng["bus"]["54"]
--- script.jl
+++ script.jl
@@ -1,11 +1,13 @@
-using JSON
-using PowerModelsDistribution
-import Ipopt
+using Distributed
+addprocs(4)
+@everywhere using JSON
+@everywhere using PowerModelsDistribution
+@everywhere import Ipopt
 using PyCall
-using JuMP
+@everywhere using JuMP
 
 # Include local modules
-include("helpers.jl")
+@everywhere include("helpers.jl")
 
 """
 Stuff that needs to be done:
@@ -13,8 +15,10 @@ Stuff that needs to be done:
 * need to figure out how to run results and dump them efficiently into a file
 """
 network_name = "J"
-NUM_TS_ITERATIONS = 161280
-CHUNK_SIZE = 1
+NUM_TS_ITERATIONS = 5
+CHUNK_SIZE = 2
+RUN_DISTRIBUTED = false
+DEFAULT_VBASE = 0.230
 
 network_path = joinpath((pwd(), "networks", network_name, "Master.dss"))
 
@@ -23,12 +27,21 @@ network_model = parse_file(network_path)
 # Remove the existing voltage bounds
 remove_all_bounds!(network_model)
 
+
+network_model["settings"]["vbases_default"] = Dict{String,Real}()
+# Add explicit base voltage to each bus to prevent calc voltage bases shenanigans
+vbases = network_model["settings"]["vbases_default"]
+for bus_id in keys(network_model["bus"])
+    vbases[bus_id] = DEFAULT_VBASE
+end
+
 # Add back the voltage bounds we care about
 add_bus_absolute_vbounds!(
     network_model,
     phase_lb_pu=0.9,
     phase_ub_pu=1.1,
 )
+
 
 move_loads_to_pp_phase(network_model, network_name)
 pp_model = load_pp_pickle_model_sgen_json(network_name)
@@ -40,12 +53,6 @@ add_solar_network_model(network_model, pp_model, network_name)
 network_model["voltage_source"]["source"]["cost_pg_parameters"] = [0, 1, 0]
 
 
-
-
-
-# write_json(sol_eng, "res.json")
-
-start_index = 1
 data_base_path = joinpath(pwd(), "input_data")
 pv_path = joinpath(data_base_path, "test_pv.csv")
 active_path = joinpath(data_base_path, "test_active.csv")
@@ -65,26 +72,38 @@ pv_df = pv_df .* 1000
 active_df = active_df .* 1000
 reactive_df = reactive_df .* 1000
 
-# for range_end = range(CHUNK_SIZE, stop=NUM_TS_ITERATIONS, step=CHUNK_SIZE)
+@everywhere function run_ts_model(network_name::String, network_model::Dict{String,Any}, start_index, end_index, active_df, reactive_df, pv_df)
+    add_time_series(network_name, network_model, active_df, reactive_df, pv_df, start_index, end_index)
+    data_math = transform_data_model(network_model, kron_reduce=false, phase_project=false, multinetwork=true)
+    add_start_vrvi!(data_math)
+    model = instantiate_mc_model(data_math, IVRENPowerModel, build_mn_mc_opf)
+    res = optimize_model!(model, optimizer=Ipopt.Optimizer)
+    res_math = res["solution"]
 
-add_time_series(network_name, network_model, active_df, reactive_df, pv_df, 1800, 1802)
+    sol_eng = transform_solution(res_math, data_math, make_si=true)
+    write_results(network_name, sol_eng, "res-$end_index")
+    sol_eng
+end
 
-# Convert to mathematical representation
-data_math = transform_data_model(network_model, kron_reduce=false, phase_project=false, multinetwork=true)
 
 
-# Infer no load voltages
-add_start_vrvi!(data_math)
+if RUN_DISTRIBUTED
+    @sync @distributed for range_end = range(start=CHUNK_SIZE, stop=NUM_TS_ITERATIONS, step=CHUNK_SIZE)
+        start_idx = range_end - (CHUNK_SIZE - 1)
+        run_ts_model(network_name, network_model, start_idx, range_end, active_df, reactive_df, pv_df)
+        nothing
+    end
+else
+    print("running serial\n")
+    for range_end = range(start=CHUNK_SIZE, stop=NUM_TS_ITERATIONS, step=CHUNK_SIZE)
+        start_idx = range_end - (CHUNK_SIZE - 1)
+        print(range_end)
+        print("\n")
+        sol_eng = run_ts_model(network_name, network_model, start_idx, range_end, active_df, reactive_df, pv_df)
+        write_json(sol_eng, "res-10.json")
+        # TODO: remove this
+        nothing
+        break
+    end
+end
 
-model = instantiate_mc_model(data_math, IVRENPowerModel, build_mn_mc_opf)
-res = optimize_model!(model, optimizer=Ipopt.Optimizer)
-res_math = res["solution"]
-
-sol_eng = transform_solution(res_math, data_math, make_si=true)
-
-# Start from next index to go
-# global start_index = range_end + 1
-# break
-# end
-
-write_json(sol_eng, "res.json")
